61c45a07ff02ded49770d77988da1295c40e9786
Georgia Betina Haritsch <gharitsch@softexpert.com>
2023-03-21 13:43:50 -0300

    add files

--- restAPI/index.php
+++ restAPI/index.php
@@ -12,13 +12,14 @@
 	require "./src/controller/TipoProdutoController.php";
 	require "./src/Database.php";
 	require "./src/ErrorHandler.php";
+	require "./cors.php";
 	
 	set_error_handler("ErrorHandler::handleError");
 	set_exception_handler("ErrorHandler::handleException");
 	
 	header("Content-type: application/json; charset=UTF-8");
 	header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+	header("Access-Control-Allow-Methods: GET, POST, PATCH, OPTIONS");
 	header("Access-Control-Allow-Headers: Origin, Content-Type, Accept");
 
 	$parts = explode("/", $_SERVER["REQUEST_URI"]);
@@ -55,6 +56,13 @@
 		default:
 			http_response_code(404);
 			exit;
+	}
+
+	if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Methods: GET, POST");
+		header("Access-Control-Allow-Headers: Content-Type");
+		exit;
 	}
 
 	$id = $parts[4] ?? null;
